cfe9cc05bb5465eab3f84956c90d0a80e8a87a83
747-diego <725@holbertonschool.com>
2020-03-11 04:03:04 +0000

    [TESTING]

--- level_0/hodor0.py
+++ level_0/hodor0.py
@@ -2,4 +2,4 @@
 import requests
 PayLoads = {'id':'823', 'holdthedoor':'Submit'}
 for index in range(1, 1024):
-    p = requests.post('http://158.69.76.135/level0.php', data = PayLoads)
+    p = requests.post('http://158.69.76.135/hodor0.php', data = PayLoads)
