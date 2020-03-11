03c5225cd6f12eb29e4feceef6287dd4fd409001
747-diego <725@holbertonschool.com>
2020-03-11 04:11:23 +0000

    [TESTING]

--- level_0/hodor0.py
+++ level_0/hodor0.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-import requests
-PayLoads = {"id":"725", "holdthedoor":"Submit"}
-for index in range(1, 1024):
-    p = requests.post("http://158.69.76.135/level0.php", data = PayLoads)
+if __name__ == "__main__":
+    import requests
+
+    API_ENDPOINT = "http://158.69.76.135/level0.php"
+    info = {"id":"725", "holdthedoor":"Submit+Query"}
+    for index in range(0, 1020):
+        payload = requests.post(url = API_ENDPOINT, data = info)
