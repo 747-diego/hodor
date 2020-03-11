96f94f00a429a28151f869f5248557605080db13
747-diego <725@holbertonschool.com>
2020-03-11 04:22:10 +0000

    [TESTING]

--- level_0/hodor0.py
+++ level_0/hodor0.py
@@ -3,6 +3,5 @@ if __name__ == "__main__":
     import requests
     from bs4 import BeautifulSoup
 
-    info = {"id":725, "holdthedoor":"Submit"}
     for index in range(1024):
-        payload = requests.post("http://158.69.76.135/level0.php", data=info)
+        payload = requests.post("http://158.69.76.135/level0.php", data={"id":725, "holdthedoor":"Submit"})
